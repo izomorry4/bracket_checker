1a9eb3a95b028aa0dd716623fe8c780d3d71eca2
izomorry4 <izomorry4@yandex.ru>
2018-01-22 21:42:03 +0300

    namespace

--- BracketChecker.php
+++ BracketChecker.php
@@ -1,39 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: izomo
- * Date: 08.01.2018
- * Time: 15:38
- */
-namespace Izomorry4\Brackets;
+namespace Izomorry4\BracketChecker {
 
-use InvalidArgumentException;
+    use InvalidArgumentException;
 
-class BracketChecker
-{
-    const BRACKET_LIST = array("(",")");
-    const ALLOW_LIST = array('\x0a','\x09','\x0d','\x20','\x28','\x29');
+    class BracketChecker
+    {
+        const BRACKET_LIST = array("(",")");
+        const ALLOW_LIST = array('\x0a','\x09','\x0d','\x20','\x28','\x29');
 
-    /**
-     * @param $str
-     */
-    private static function checkAllowList($str){
-        $pattern = '/[^'.implode(self::ALLOW_LIST).']/';
-        if(preg_match($pattern,$str))
-            throw new InvalidArgumentException("Incorrect symbol in input string");
-    }
-
-    /**
-     * @param string $str
-     * @return bool
-     */
-    public static function IsCorrect(string $str){
-        self::checkAllowList($str);
-        $k = 0;
-        for($i = 0;$i < strlen($str);$i++){
-            if(in_array($str[$i],self::BRACKET_LIST))
-                $k += $str[$i] == "(" ? 1 : -1;
+        /**
+         * @param $str
+         */
+        private static function checkAllowList($str){
+            $pattern = '/[^'.implode(self::ALLOW_LIST).']/';
+            if(preg_match($pattern,$str))
+                throw new InvalidArgumentException("Incorrect symbol in input string");
         }
-        return $k == 0;
+
+        /**
+         * @param string $str
+         * @return bool
+         */
+        public static function IsCorrect(string $str){
+            self::checkAllowList($str);
+            $k = 0;
+            for($i = 0;$i < strlen($str);$i++){
+                if(in_array($str[$i],self::BRACKET_LIST))
+                    $k += $str[$i] == "(" ? 1 : -1;
+            }
+            return $k == 0;
+        }
     }
 }
