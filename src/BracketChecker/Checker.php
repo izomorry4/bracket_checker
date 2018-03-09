f729373e0af7c150e34b377895889bc4236e575f
izomorry4 <izomorry4@yandex.ru>
2018-03-09 11:37:48 +0300

    exceptions

--- src/BracketChecker/Checker.php
+++ src/BracketChecker/Checker.php
@@ -1,27 +1,29 @@
 <?php
 namespace Izomorry4\BracketChecker;
 
-use InvalidArgumentException;
+use Izomorry4\Exceptions;
 
 class Checker
 {
-    const BRACKET_LIST = array("(",")");
-    const ALLOW_LIST = array('\x0a','\x09','\x0d','\x20','\x28','\x29');
+    const BRACKET_LIST = ["(",")"];
+    const ALLOW_LIST = ['\x0a','\x09','\x0d','\x20','\x28','\x29'];
 
     /**
      * @param $str
      */
-    private static function checkAllowList($str){
+    private static function checkAllowList($str)
+    {
         $pattern = '/[^'.implode(self::ALLOW_LIST).']/';
         if(preg_match($pattern,$str))
-            throw new InvalidArgumentException("Incorrect symbol in input string");
+            throw new Exceptions\ArgumentException("Incorrect symbol in input string");
     }
 
     /**
      * @param string $str
      * @return bool
      */
-    public static function IsCorrect(string $str){
+    public static function IsCorrect(string $str)
+    {
         self::checkAllowList($str);
         $k = 0;
         for($i = 0;$i < strlen($str);$i++){
---
+++ src/Exceptions/ArgumentException.php
@@ -0,0 +1,9 @@
+<?php
+namespace Izomorry4\Exceptions;
+
+use InvalidArgumentException;
+
+class ArgumentException extends InvalidArgumentException
+{
+
+}
